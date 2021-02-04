9195c31282812154b409a040f1e864479045616f
Yogesh Kumar <yogeshykk5@gmail.com>
2021-02-04 08:37:05 +0530

    Initial Commit

---
+++ src/main/java/com/way2solution/config/MySpringMvcDispatcherServletInitilizer.java
@@ -0,0 +1,23 @@
+package com.way2solution.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {AppConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+    </head>
+    <body>
+    <h2>Home Page</h2>
+    </body>
+</html>
